2279e070d233ad6ea6da7a22c2a72098a78acb5e
appMaker48167 <jasonacrandall@gmail.com>
2025-08-11 07:27:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Public repo recommended for simplest setup.
 // For private repos, see README for a secure proxy approach.
 const CONFIG = {
-  REPO_OWNER: "your-github-username",
+  REPO_OWNER: "appMaker48167",
   REPO_NAME: "sendnetwork-resources",
   BRANCH: "main",
   GITHUB_TOKEN: null
